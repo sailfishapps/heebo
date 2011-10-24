491393fdf0ff6f095770bd2a336311d5ff4d9698
Mats Sjöberg <mats@sjoberg.fi>
2011-10-24 21:15:37 +0300

    Added gitorious link to about page.

--- src/js/constants.js
+++ src/js/constants.js
@@ -32,7 +32,7 @@ var heebo_copyright
     = "Copyright 2011 &copy; Mats Sjöberg, Niklas Gustafsson";
 
 var code_license =
-    "All the source code and game level maps for Heebo are licensed under GPLv3.<br/><br/>"+
+    "All the source code and game level maps for Heebo are licensed under GPLv3.<br/><br/>The source code can be downloaded from <a href=\"http://gitorious.org/heebo\">http://gitorious.org/heebo</a><br/><br/>"+
     "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.<br/><br/>"+
     "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.<br/><br/>"+
     "You should have received a copy of the GNU General Public License along with this program.  If not, see <a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>.";
