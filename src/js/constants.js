b8bc84c267a8a45e867481cec20058bd2dc65661
Mats Sjöberg <mats@sjoberg.fi>
2012-03-11 17:00:19 +0200

    Added help text on double locks.

--- src/js/constants.js
+++ src/js/constants.js
@@ -129,7 +129,7 @@ var heebo_help_topic_3 = "Bonus sweetie";
 var heebo_help_3 = "If you explode more than three blocks in a line, a bonus background tile with a same colour block on it will be turned into gold. This is handy to get to those last hard-to-reach tiles!";
 
 var heebo_help_topic_4 = "Locked blocks";
-var heebo_help_4 = "In the later levels some blocks are locked and can't be moved.  Free them by matching them up with two or more other blocks of the same colour!";
+var heebo_help_4 = "In the later levels some blocks are locked and can't be moved.  Free them by matching them up with two or more other blocks of the same colour! Some times there are two locks, then you need to match them up twice.";
 
 var heebo_help_5 = "Now go flip some blocks!!";
 
